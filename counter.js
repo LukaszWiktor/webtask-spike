0bdd03cab1e8cb571c6cee8727ac4c73804d4389
Łukasz Wiktor <lukasz.wiktor@gmail.com>
2017-04-11 07:50:16 +0200

    counter

---
+++ counter.js
@@ -0,0 +1,15 @@
+module.exports = function (ctx, cb) {
+    ctx.storage.get(function (err, data) {
+        if (err) return cb(err);
+        if (!data) data = { };
+        if (!data.counter) data.counter = 0;
+        
+        data.counter++;
+        
+        ctx.storage.set(data, function (err) {
+            if (err) return cb(err);
+            
+            cb(null, data);
+        });
+    });
+}
